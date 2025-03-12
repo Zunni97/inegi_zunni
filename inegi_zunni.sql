f3f9b04d4bb4d6a2d572214f0d9acae877a6ea4b
Zunni97 <115298043+Zunni97@users.noreply.github.com>
2025-03-11 20:29:36 -0700

    Archivo SQL final

---
+++ inegi_zunni.sql
@@ -0,0 +1,186 @@
+USE [master]
+GO
+/****** Object:  Database [inegi_zunni]    Script Date: 11/03/2025 08:10:25 p. m. ******/
+CREATE DATABASE [inegi_zunni]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'inegi_zunni', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\inegi_zunni.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'inegi_zunni_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\inegi_zunni_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [inegi_zunni] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [inegi_zunni].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [inegi_zunni] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET AUTO_CLOSE ON 
+GO
+ALTER DATABASE [inegi_zunni] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [inegi_zunni] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [inegi_zunni] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [inegi_zunni] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [inegi_zunni] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [inegi_zunni] SET  MULTI_USER 
+GO
+ALTER DATABASE [inegi_zunni] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [inegi_zunni] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [inegi_zunni] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [inegi_zunni] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [inegi_zunni] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [inegi_zunni] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [inegi_zunni] SET QUERY_STORE = OFF
+GO
+USE [inegi_zunni]
+GO
+/****** Object:  Table [dbo].[Actividades_Economicas]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Actividades_Economicas](
+	[codigo_act] [int] NOT NULL,
+	[nombre_act] [varchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[codigo_act] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Contacto]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Contacto](
+	[id_Contac] [int] NOT NULL,
+	[telefono] [varchar](25) NULL,
+	[correo] [varchar](255) NULL,
+	[sitio_web] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_Contac] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Establecimiento]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Establecimiento](
+	[id_estab] [int] NOT NULL,
+	[nom_estab] [varchar](255) NOT NULL,
+	[raz_social] [varchar](255) NULL,
+	[codigo_act] [int] NOT NULL,
+	[fecha_alta] [varchar](255) NOT NULL,
+	[id_Contac] [int] NULL,
+	[id_Ubicac] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_estab] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Localidad]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Localidad](
+	[cve_loc] [int] NOT NULL,
+	[localidad] [varchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[cve_loc] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Municipio]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Municipio](
+	[cve_municipio] [int] NOT NULL,
+	[municipio] [varchar](45) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[cve_municipio] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Ubicaciones]    Script Date: 11/03/2025 08:10:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Ubicaciones](
+	[id_Ubicac] [int] NOT NULL,
+	[tipo_vial] [varchar](255) NULL,
+	[numero_ext] [int] NULL,
+	[edificio] [varchar](255) NULL,
+	[tipo_asent] [varchar](255) NULL,
+	[nomb_asent] [varchar](255) NULL,
+	[cod_postal] [int] NULL,
+	[cve_mun] [int] NOT NULL,
+	[cve_loc] [int] NOT NULL,
+	[latitud] [decimal](18, 0) NOT NULL,
+	[longitud] [decimal](18, 0) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_Ubicac] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [inegi_zunni] SET  READ_WRITE 
+GO
